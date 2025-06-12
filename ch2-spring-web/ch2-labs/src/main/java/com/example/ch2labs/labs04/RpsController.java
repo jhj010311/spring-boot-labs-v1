9a922764dd1875b22ee8073c926a82b967a35565
DESKTOP-GR883KV\시작메뉴고쳐 <jhj010311@gmail.com>
2025-06-12 10:38:56 +0900

    250611 보충

--- ch2-spring-web/ch2-labs/src/main/java/com/example/ch2labs/labs04/RpsController.java
+++ ch2-spring-web/ch2-labs/src/main/java/com/example/ch2labs/labs04/RpsController.java
@@ -10,19 +10,56 @@ import java.util.Map;
 
 @Controller
 public class RpsController {
-    enum RPS = {
-            ROCK
-    }
 
     @GetMapping("/rps")
     @ResponseBody
     public Map<String, String> rps(@RequestParam String user) {
         Map<String, String> response = new HashMap<>();
-        
+
+        int comRps = (int) Math.floor(Math.random() * 3 + 1);
+        String server = "";
+
+        switch (comRps) {
+            case 1:
+                server = "rock";
+                break;
+            case 2:
+                server = "scissors";
+                break;
+            case 3:
+                server = "paper";
+                break;
+            default:
+                server = "error";
+        }
+
+        String result = "";
+
+        if(user.equals(server)) {
+            result = "무승부";
+        } else if (user.equals("rock")) {
+            if(server.equals("scissors")) {
+                result = "승리!";
+            } else {
+                result = "패배...";
+            }
+        } else if (user.equals("scissors")) {
+            if(server.equals("paper")) {
+                result = "승리!";
+            } else {
+                result = "패배...";
+            }
+        } else if (user.equals("paper")) {
+            if(server.equals("rock")) {
+                result = "승리!";
+            } else {
+                result = "패배...";
+            }
+        }
 
         response.put("user", user);
-        //response.put("server", user);
-        //response.put("result", user);
+        response.put("server", server);
+        response.put("result", result);
 
 
         return response;
